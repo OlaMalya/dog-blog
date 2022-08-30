961897f1dc8578d737a52ef2643005da36c3dbc5
OlaMalya <profuchet@mail.ru>
2022-08-30 14:00:12 +0300

    added in views post Ночь

--- project/blog/views.py
+++ project/blog/views.py
@@ -9,5 +9,6 @@ def post_lists(request):
     {'name': 'Утро', 'text': 'Доброе утро'},
     {'name': 'День', 'text': 'Добрый день'},
     {'name': 'Вечер', 'text': 'Добрый вечер'},
+    {'name': 'Ночь', 'text': 'Добрый ночь'},
 ]
     return render(request,'blog/index.html',context={'posts':posts})
